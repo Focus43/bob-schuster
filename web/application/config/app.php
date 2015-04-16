916b9cc0e517955e7e2567f7726cc82a0b464994
Jonathan Hartman <jhartman86@gmail.com>
2015-04-16 17:08:12 -0600

    updat

--- web/application/config/app.php
+++ web/application/config/app.php
@@ -1,8 +1,9 @@
 <?php
 
-return array(
-    // 'providers' => array(
-    //     'core_session'  => '\Application\Src\Session\SessionServiceProvider',
-    //     'core_database' => '\Application\Src\Database\DatabaseServiceProvider'
-    // )
-);
+return array();
+// return array(
+//     'providers' => array(
+//         'core_session'  => '\Application\Src\Session\SessionServiceProvider',
+//         'core_database' => '\Application\Src\Database\DatabaseServiceProvider'
+//     )
+// );
