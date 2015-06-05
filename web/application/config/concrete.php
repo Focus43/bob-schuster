704639561c5da42446e6d35d75f2b15dfd08f9ba
Jonathan Hartman <jhartman86@gmail.com>
2015-06-05 13:16:09 -0600

    configs

--- web/application/config/concrete.php
+++ web/application/config/concrete.php
@@ -17,6 +17,26 @@ $redisStashCacheDriver = array(
     )
 );
 
+$trackingCode = <<<EOT
+<script type="text/javascript">
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-24748273-2']);
+	_gaq.push(['_gat._forceSSL']);
+	_gaq.push(['_trackPageview']);
+
+	(function () {
+		var ga = document.createElement('script');
+		ga.type = 'text/javascript';
+		ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
+	})();
+
+</script>
+EOT;
+
 return array(
     'seo' => array(
         'url_rewriting' => true,
@@ -36,26 +56,10 @@ return array(
     'misc'  => array(
         'seen_introduction' => true
     ),
-    'tracking' => array(
-        'code' => <<<EOT
-<script type="text/javascript">
-
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-24748273-2']);
-	_gaq.push(['_gat._forceSSL']);
-	_gaq.push(['_trackPageview']);
-
-	(function () {
-		var ga = document.createElement('script');
-		ga.type = 'text/javascript';
-		ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(ga, s);
-	})();
-
-</script>
-EOT
+    'seo' => array(
+        'tracking' => array(
+            'code' => $trackingCode
+        )
     ),
     'email' => array(
         'default' => array(
