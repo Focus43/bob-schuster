4cc104661dd5aaf3109d6ed995079ddea1526c99
Jonathan Hartman <jhartman86@gmail.com>
2015-06-05 13:13:41 -0600

    Update

--- web/application/config/concrete.php
+++ web/application/config/concrete.php
@@ -36,6 +36,27 @@ return array(
     'misc'  => array(
         'seen_introduction' => true
     ),
+    'tracking' => array(
+        'code' => <<<EOT
+<script type="text/javascript">
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-24748273-2']);
+	_gaq.push(['_gat._forceSSL']);
+	_gaq.push(['_trackPageview']);
+
+	(function () {
+		var ga = document.createElement('script');
+		ga.type = 'text/javascript';
+		ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
+	})();
+
+</script>
+EOT
+    ),
     'email' => array(
         'default' => array(
             'address' => 'hi@focus-43.com',
