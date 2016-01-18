4d00c3d406c283659dc0e029993dcc582ce092e7
Sam Fitz <abigwonderful@gmail.com>
2016-01-18 11:27:49 -0700

    update db creds for mt setup

--- web/application/config/database.php
+++ web/application/config/database.php
@@ -1,15 +1,28 @@
 <?php
-
 return array(
     'default-connection' => 'concrete',
     'connections' => array(
         'concrete' => array(
             'driver'    => 'c5_pdo_mysql',
-            'server'    => $_SERVER['DATABASE1_HOST'],
-            'database'  => $_SERVER['DATABASE1_NAME'],
-            'username'  => $_SERVER['DATABASE1_USER'],
-            'password'  => $_SERVER['DATABASE1_PASS'],
+            'server'    => 127.0.0.1,
+            'database'  => 'bobschus_main',
+            'username'  => 'bobschus_admin',
+            'password'  => 'Focus43#2016',
             'charset'   => 'utf8'
         )
     )
 );
+
+// return array(
+//     'default-connection' => 'concrete',
+//     'connections' => array(
+//         'concrete' => array(
+//             'driver'    => 'c5_pdo_mysql',
+//             'server'    => $_SERVER['DATABASE1_HOST'],
+//             'database'  => $_SERVER['DATABASE1_NAME'],
+//             'username'  => $_SERVER['DATABASE1_USER'],
+//             'password'  => $_SERVER['DATABASE1_PASS'],
+//             'charset'   => 'utf8'
+//         )
+//     )
+// );
