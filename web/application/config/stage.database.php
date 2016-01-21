1f2a638f88db7c2a4ef4d9851917e797d9e8449e
Sam Fitz <abigwonderful@gmail.com>
2016-01-21 15:51:05 -0700

    db connection update for staging

--- web/application/config/stage.database.php
+++ web/application/config/stage.database.php
@@ -6,7 +6,7 @@ return array(
             'driver'    => 'c5_pdo_mysql',
             'server'    => '127.0.0.1',
             'database'  => 'bobschus_stage',
-            'username'  => 'bobschus_stage',
+            'username'  => 'bobschus_admin',
             'password'  => 'Focus43#2016',
             'charset'   => 'utf8'
         )
